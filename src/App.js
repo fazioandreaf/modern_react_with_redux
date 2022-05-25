4d73372fbb831f86d723737d9495838e1c612117
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2022-05-25 11:22:37 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Fragment, useState, useReducer, useContext} from 'react';
+import './App.css';
+
+
+function App() {
+
+	return (
+		<Fragment>
+			<div className='container'>
+					<h2>PropDrilling</h2>
+			</div>
+		</Fragment>
+	)
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+reactDom.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
