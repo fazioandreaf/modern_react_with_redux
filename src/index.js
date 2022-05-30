aadb63b3f88b467d1e690e8575127076c5862d90
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2022-05-30 15:06:29 +0200

    SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,46 +1,7 @@
 // import the React and RactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
-
-class App extends React.Component {
-	state = { lat: null, errorMessage: ''};
-
-	componentDidMount() {
-		console.log('My component did mount')
-		window.navigator.geolocation.getCurrentPosition(
-			position => {
-				// we called sestate!!
-				this.setState({lat: position.coords.latitude});
-			},
-			err => {this.setState({errorMessage: err.message})}
-		);
-	}
-
-	componentDidUpdate() {
-		console.log('My component did update')
-	}
-
-	renderContent() {
-		if (!this.state.errorMessage && this.state.lat ) {
-			return (<SeasonDisplay lat={this.state.lat} />);
-		} else if (this.state.errorMessage && !this.state.lat) {
-			return (<div> Error: {this.state.errorMessage}</div>);
-		} else {
-			return (<Spinner message='Please accept location request' />);
-		}
-	}
-
-	// React says we have to define render!!
-    render() {
-		return (
-			<div className='border red'>
-				{this.renderContent()}
-			</div>
-		)
-    }
-}
+import App from './components/App';
 
 // Take the react component and show it on the screen
 ReactDOM.render(
