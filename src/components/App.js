41394153837484ca0326e02decdeba35b98bf17b
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2022-05-30 16:34:08 +0200

    Api unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import axios from 'axios';
 
-const App =() => {
+class App extends React.Component {
+    state = {images: []}
 
-    
-    return (
-        <div className="ui container">
-            <SearchBar />
-        </div>
-    )
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos',{
+            headers: {
+                Authorization: 'Client-ID 5c34t4jdyYj75Q5SiR-XEB_RvDEQQXvb0GUoAcXeGFw'
+            },
+            params: {query: term}
+        });
+
+        this.setState({images: response.data.results})
+        this.state.images.map((elem) => {})
+    }
+
+    render () {
+        return (
+            <div className="ui container">
+                <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
+            </div>
+        )
+    }
 }
 
 export default App
