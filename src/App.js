4288a68e16a7d39734168c068fbbc41c0cfd0828
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2022-06-01 09:54:27 +0200

    dangerouslySetInnerHTML

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
     {
@@ -20,6 +21,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
+                <Search />
                 <Accordion items={items} />
             </div>
         )
---
+++ src/components/Search.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const Search = () => {
+    const [term, setTerm] = useState('')
+    const [results, setResults] = useState([])
+
+    useEffect(() => {
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+            setResults(data.query.search);
+        }
+
+        if (term) {
+            search();
+        }
+    }, [term])
+
+    const renderedResults = results.map(({title, snippet, pageid}) => {
+        return (
+            <React.Fragment key={pageid}>
+                <div className="item">
+                    <div className="right floated content">
+                        <a href={`https>//en.wikipedia.org?curid=${pageid}`} className="ui button"> Go</a>
+                    </div>
+                    <div className="content">
+                        <div className="header">
+                            {title}
+                        </div>
+                        <span dangerouslySetInnerHTML={{__html: snippet}}></span>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    })
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label htmlFor="text">Enter search term</label>
+                    <input
+                        value={term}
+                        onChange={(e) => setTerm(e.target.value)}
+                        type="text"
+                        className="input"
+                    />
+                </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
+            </div>
+        </div>
+    )
+}
+
+export default Search
